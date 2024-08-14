201e61df6e219bc698e60418c63b8de56ed10702
Arnav K <arnavkaushal09@gmail.com>
2024-08-15 01:24:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       allowedOrigins: [
         "scaling-cod-g66jw4j4v6xf9gpp-3000.app.github.dev",
         "localhost:3000",
-        "chapters_as_tasks.vercel.app",
+        "chapters-as-tasks.vercel.app",
       ],
     },
   },
